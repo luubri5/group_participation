6671ff4a2f9861373b3b5d456a6e396df571d1c2
alliL <allison.lee124@gmail.com>
2018-05-09 15:07:36 -0700

    Add widget. Close #2.

--- app.R
+++ app.R
@@ -1,8 +1,12 @@
 library(shiny)
 #
 ui <- fluidPage(
-  h1("Brian is cool"),
-  img("", src = "https://media2.giphy.com/media/l3q2Ip7FrmPE33EiI/giphy.gif")
+  h1("Brian is not cool"),
+  img("", src = "https://media2.giphy.com/media/l3q2Ip7FrmPE33EiI/giphy.gif"),
+  sliderInput(
+    inputId = "slider",
+    label = "My First Slider", min = 0, max = 100, value = 50, step = 10
+  )
   )
 
 server <- function(input, output) {
